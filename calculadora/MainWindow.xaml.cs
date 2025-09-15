3db8334c24d5ee42b0d058501f98fd05b97a6ae6
pablo.serrano_alumno <pablo.serrano_alumno@DESKTOP-HE4DH9G>
2025-09-15 19:32:38 +0200

    Agregar archivos de proyecto.

---
+++ calculadora/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace calculadora
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void botn_Click(object sender, RoutedEventArgs e)
+        {
+           int n1 =(int.Parse(txt.Text));
+           int n2 = (int.Parse(txt2.Text));
+           
+            lbl.Content = (n1 + n2).ToString();
+        }
+    }
+}
